7b4a56cd8b8474a12f3bc29b7fac8461fd2ef5fd
cookie_gg <cookie.gg.dev@gmail.com>
2023-03-08 13:26:55 +0900

    implement grpc server with nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { AppServiceController, SampleDataById, SampleData } from 'proto/sample';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+export class AppController implements AppServiceController {
+  @GrpcMethod('AppService')
+  findOne(data: SampleDataById): SampleData {
+    const items: SampleData[] = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Doe' },
+    ];
+    const filteredItems = items.filter((item) => item.id === data.id);
+    return filteredItems.length > 0 ? filteredItems[0] : null;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<GrpcOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: 'localhost:5000',
+      package: 'sample',
+      protoPath: join(process.cwd(), 'proto/sample.proto'),
+    },
+  });
+  await app.listen();
 }
 bootstrap();
